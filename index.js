d0f799716aab356f379a9cc3709392f39815742e
kunisho <kuni.sho.0805@gmail.com>
2018-11-15 15:04:26 +0900

    html css js ファイル分割

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
 
+
   var ac = document.getElementById("ac"); // canvas要素のオブジェクトを取得
   ac.ontouchstart = function (e) {
-    e.preventDefault();     // デフォルトイベントをキャンセル
-    var s = "";             // 変数sを初期化
+      e.preventDefault();     // デフォルトイベントをキャンセル
+      var s = "";             // 変数sを初期化
 
-    // 引数のtouchesプロパティは配列の要素数（触れている指の数）だけ繰り返し処理
-    for (var i = 0; i < e.touches.length; i++) {
-      var t = e.touches[i];       // 触れている指に関する情報を取得
+      var arrayX = [];
+      var arrayY = [];
 
-      s += "[" + i + "]";
-      s += "x=" + t.pageX + ",";
-      s += "y=" + t.pageY + "<br>";
-    }
-    document.getElementById("disp").innerHTML = s;  // 生成した文字列を画面に表示
+
+      // 引数のtouchesプロパティは配列の要素数（触れている指の数）だけ繰り返し処理
+      for (var i = 0; i < e.touches.length; i++) {
+          var t = e.touches[i];       // 触れている指に関する情報を取得
+
+          s += "[" + i + "]";
+          s += "x=" + t.pageX + ",";
+          s += "y=" + t.pageY + "<br>";
+
+          arrayX.push(t.pageX);
+          arrayY.push(t.pageY);
+          //console.log(array); // ['a', 'b', 'c']
+      }
+      document.getElementById("disp").innerHTML = s;  // 生成した文字列を画面に表示
+
+      var ctx = document.getElementById("ac").getContext("2d");
+
+
+
+      ctx.strokeStyle = "white";  //線の色を緑に指定
+      ctx.fillStyle = "white";  //塗り潰しの色を赤に指定
+      ctx.strokeRect(0,0,0,0);
+      ctx.fillRect(0,0,1000, 600);
+
+      ctx.strokeStyle = "black";  //線の色を緑に指定
+        for (var i = 0; i < e.touches.length; i++) {
+        ctx.beginPath();
+        ctx.arc(arrayX[i], arrayY[i], 50, 0, Math.PI*2, true);
+        ctx.stroke();
+
+      }
+
   };
