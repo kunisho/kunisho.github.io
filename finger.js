e6a6fd6bd40bbb2bdc63f735d7e92de5ade88fae
kunisho <kuni.sho.0805@gmail.com>
2018-11-16 15:11:42 +0900

    戻した

--- finger.js
+++ finger.js
@@ -15,23 +15,31 @@ window.onload = function(){
       let arrayX = [];
       let arrayY = [];
 
+
+      // 引数のtouchesプロパティは配列の要素数（触れている指の数）だけ繰り返し処理
+      for (let i = 0; i < e.touches.length; i++) {
+          let t = e.touches[i];       // 触れている指に関する情報を取得
+
+          s += "[" + i + "]";
+          s += "x=" + t.pageX + ",";
+          s += "y=" + t.pageY + "<br>";
+
+          arrayX.push(t.pageX);
+          arrayY.push(t.pageY);
+          //console.log(array); // ['a', 'b', 'c']
+      }
+      document.getElementById("disp").innerHTML = s;  // 生成した文字列を画面に表示
+
       let ctx = document.getElementById("cv").getContext("2d");
+
+
 
       ctx.strokeStyle = "white";  //線の色を緑に指定
       ctx.fillStyle = "white";  //塗り潰しの色を赤に指定
       ctx.strokeRect(0,0,0,0);
       ctx.fillRect(0,0,1000, 600);
 
-      // 引数のtouchesプロパティは配列の要素数（触れている指の数）だけ繰り返し処理
-      for (let i = 0; i < e.touches.length; i++) {
-          let t = e.touches[i];       // 触れている指に関する情報を取得
-
-          arrayX.push(t.pageX);
-          arrayY.push(t.pageY);
-          //console.log(array); // ['a', 'b', 'c']
-      }
-
-      let OrderNum=[];//１から順番に指の数だけ数字が入っている配列[1,2,3,4,5....]
+      let OrderNum=[];
       for(let i = 1; i < e.touches.length+1; i++){
         OrderNum.push(i);
       }
@@ -54,14 +62,17 @@ window.onload = function(){
           ctx.stroke();
       }
 
-      let touchNum =[];//それぞれの指の番号をcanvasの下にテキストとして表示するための臨時変数
+
+
+
+
+
+
 
       for (let i = 0; i < e.touches.length; i++) {
         let random = Math.floor(Math.random() * OrderNum.length); // 0~触っている指の数の値
 
         let txt = OrderNum[random]; //描画する文字
-        touchNum.push(OrderNum[random]);
-
 
         OrderNum.splice(random, 1);
 
@@ -71,19 +82,7 @@ window.onload = function(){
         // let rad = Math.atan2(500-arrayX[i], arrayY[i]-300);
         // ctx.rotate(Math.PI / 4); //回転
         ctx.fillText(txt,arrayX[i],arrayY[i]-60);
-
-
-                  s += "[" + i + "]";
-                  s += "x=" + t.pageX + ",";
-                  s += "y=" + t.pageY + ",";
-                  s += "Num=" + touchNum[i] + "<br>";
-
-
       }
-            document.getElementById("disp").innerHTML = s;  // 生成した文字列を画面に表示
-
-
-
 
   };
 
